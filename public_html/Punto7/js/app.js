05fe82c89231c3b4f9b858c475d2fbb101dc1408
Joan Serna <jsernaleiton@gmail.com>
2017-07-30 12:02:16 -0500

    Punto 7 terminado

---
+++ public_html/Punto7/js/app.js
@@ -0,0 +1,12 @@
+let number1 = prompt('Enter one number');
+let number2 = prompt('Enter other number');
+
+
+console.log(`${number1} a la ${number2}es: ${Math.pow(number1, number2)}`);
+console.log(`${number2} a la ${number1} es: ${Math.pow(number2, number1)}`);
+console.log(`The multiplication between ${number1} and ${number2} es: ${ number1 * number2}`);
+console.log(`The division between ${number1} and ${number2} es: ${ number1 / number2}`);
+console.log(`The division between ${number2} and ${number1} es: ${ number2 / number1 }`);
+console.log(`The module of the division between ${number1} and ${number2} is: ${ number1 % number2}`);
+console.log(`The subtraction between ${number1} and ${number2} is: ${ number1 - number2}`);
+console.log(`The sum between ${number1} and ${number2} is: ${ number1 + number2}`);
