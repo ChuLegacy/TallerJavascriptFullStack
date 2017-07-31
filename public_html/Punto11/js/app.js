156c947080f0b6ea2c6cdc62b40daa40f8411423
Joan Serna <jsernaleiton@gmail.com>
2017-07-30 19:57:02 -0500

    Punto 11 terminado

--- public_html/Punto11/js/app.js
+++ public_html/Punto11/js/app.js
@@ -1,4 +1,11 @@
 let x = '';
-for (let i = 0; i <= 5; i++) {
-  console.log(`${x += '*'}`);
-}
+let j = 1
+do {
+	for (let i = 0; i <= 5; i++) {
+		console.log(`${x += '*'}`);
+	}
+	j++;
+	if (j === 1) {
+		console.log(':D');
+	}
+}while(j ===1);
