e323502893b397814b2a0bc1e86c773c172ca191
Joan Serna <jsernaleiton@gmail.com>
2017-07-30 20:18:51 -0500

    Punto 12 terminado

---
+++ public_html/Punto12/js/app.js
@@ -0,0 +1,12 @@
+let x = '     *';
+let y = '*';
+let j = 1;
+while (j === 1) {
+	for (let i = 0; i < 5; i++) {
+		if (x === '     *') {
+			console.log(x);
+		}
+		console.log(x = x.slice(1) + y);
+	}
+	j++;
+}
