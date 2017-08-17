8305138b8ddc3b6f42ae809c9967774e18f735a6
ADSI <ADSI@CARDFPCTI8F228.FormacionSena.RED>
2017-08-17 17:21:23 -0500

    Punto 47 Feature en progreso Se cumplieron con los requerimientos del enunciado

---
+++ public_html/Punto47/js/app.js
@@ -0,0 +1,14 @@
+//$(() => {
+//
+//  $('#modal').modal('show');
+//  let section = $('#modal').find('section');
+//  console.log(section);
+//
+//  let asterisc = '*';
+//  do {
+//
+//  } while (asterisc.length < 7);
+//
+//  section.html('<h1>hola</h1>');
+//
+//});
