6c333b5b190fbdc91a2825c1c85ecc6c3fa39e6d
ADSI <ADSI@CARDFPCTI8F228.FormacionSena.RED>
2017-09-06 16:06:57 -0500

    Nivel dos en progreso

--- public_html/Punto48/js/app.js
+++ public_html/Punto48/js/app.js
@@ -1,17 +1,17 @@
 $(() => {
   let text = $('#text');
-  text.html = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*';
+  text.html = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; *';
   // let x = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*',
     let y = '*',
     j = 1;
   while (j === 1) {
     for (let i = 0; i < 5; i++) {
-      if (text.html === '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*') {
+      if (text.html === '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; *') {
+        debugger
         $('#text').append(`${text.html} <br>`);
-
       }
+      let tmp = text.html.substr(6) + y;
       debugger
-      let tmp = text.text().slice(1) + y;
       $('#text').append(`${text.text = tmp} <br>`);
     }
     j++;
